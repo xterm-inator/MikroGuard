84d916703b90c7e372b3101631896d56f338020b
man20820 <fatkhurrahmandjogja@gmail.com>
2024-07-31 12:33:55 +0700

    add client ip to addresslist

--- api/app/RouterOS/WireGuard.php
+++ api/app/RouterOS/WireGuard.php
@@ -5,6 +5,7 @@ namespace App\RouterOS;
 use App\RouterOS\Data\Peer;
 use App\RouterOS\Data\WireGuardInterface;
 use RouterOS\Query;
+use App\Models\User;
 
 class WireGuard extends RouterOS
 {
@@ -28,6 +29,24 @@ class WireGuard extends RouterOS
         $query = new Query('/interface/wireguard/peers/print');
         $query->where('public-key', $publicKey)
             ->where('interface', config('services.wireguard.interface'));
+
+        $response = $routerOS->client->query($query)->read();
+
+        if (!count($response)) {
+            return null;
+        }
+
+        return $response[0];
+    }
+
+    public static function getAddressList(): ?array
+    {
+        $routerOS = new self();
+
+        $user = auth()->user();
+
+        $query = new Query('/ip/firewall/address-list/print');
+        $query->where('list', $user->email);
 
         $response = $routerOS->client->query($query)->read();
 
@@ -64,6 +83,8 @@ class WireGuard extends RouterOS
     {
         $routerOS = new self();
 
+        $user = auth()->user();
+
         $query = new Query('/interface/wireguard/peers/add');
         $query->equal('allowed-address',  $peer->allowedAddress)
             ->equal('interface', $peer->interface)
@@ -71,17 +92,30 @@ class WireGuard extends RouterOS
             ->equal('preshared-key', $peer->presharedKey);
 
         $routerOS->client->query($query)->read();
+
+        $query = new Query('/ip/firewall/address-list/add');
+        $query->equal('address', $peer->allowedAddress)
+            ->equal('list', $user->email);
+
+        $routerOS->client->query($query)->read();
     }
+
 
     public static function deletePeer(string $publicKey): void
     {
         $routerOS = new self();
 
         $peer = self::getPeer($publicKey);
+        $list = self::getAddressList();
 
         if ($peer) {
             $query = new Query('/interface/wireguard/peers/remove');
             $query->equal('.id', $peer['.id']);
+
+            $routerOS->client->query($query)->read();
+
+            $query = new Query('/ip/firewall/address-list/remove');
+            $query->equal('.id', $list['.id']);
 
             $routerOS->client->query($query)->read();
         }
